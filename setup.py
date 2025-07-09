00163dedb140d1ea1dc3de386f914ae7ff3b5661
Bryant Nelson <bryant.nelson@amd.com>
2025-07-09 07:49:41 -0500

    Initial commit: Release tritonBLAS code.     Co-authored-by: Ryan Swann <ryan.swann@amd.com>     Co-authored-by: Xiaohu Guo <xiaohu.guo@amd.com>     Co-authored-by: Muhammad Osama <muhammad.osama@amd.com>     Co-authored-by: Lixun Zhang <lixun.zhang@amd.com>     Co-authored-by: Bryant Nelson <bryant.nelson@amd.com>     Co-authored-by: Stephen Brown <stephen.brown@amd.com>     Co-authored-by: Yen Ong <yen.ong@amd.com>     Co-authored-by: Pieter Ghysels <pieter.ghysels@amd.com>

---
+++ setup.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+import subprocess
+import sys
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from contextlib import contextmanager
+
+@contextmanager
+def chdir(path):
+    prev_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
+
+class CustomBuildExt(build_ext):
+    def run(self):
+        # Remove existing _origami directory if it exists
+        if os.path.exists("_origami"):
+            print("Removing existing _origami directory...")
+            shutil.rmtree("_origami")
+
+        # Clone hipBLASLt repo
+        print("Cloning hipBLASLt...")
+        subprocess.check_call([
+            "git", "clone", "--depth", "1", "--filter=blob:none", "--sparse",
+            "--branch", "develop", "https://github.com/ROCm/hipBLASLt.git", "_origami"
+        ])
+
+        # Use custom chdir context manager to run sparse-checkout
+        with chdir("_origami"):
+            subprocess.check_call([
+                "git", "sparse-checkout", "set",
+                "tensilelite/Tensile/Source/lib/source/analytical",
+                "tensilelite/Tensile/Utilities/origami",
+                "tensilelite/Tensile/Source/lib/include/Tensile/analytical"
+            ])
+
+        # Build the nested origami setup.py
+        origami_setup_path = os.path.join("_origami", "tensilelite", "Tensile", "Utilities", "origami")
+        print(f"Building origami setup.py in {origami_setup_path}...")
+        subprocess.check_call([sys.executable, "setup.py", "install"], cwd=origami_setup_path)
+
+        print("Running build_ext for main package...")
+        super().run()
+
+
+setup(
+    cmdclass={"build_ext": CustomBuildExt},
+    ext_modules = [Extension("_trigger_ext", sources=[])],
+)
